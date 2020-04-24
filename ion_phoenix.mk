e4876807d57976cfdc585ec5f9a21cd1aa77529b
Utku Ablak <62817866+utkuablak1@users.noreply.github.com>
2020-04-24 21:24:22 +0300

    ion-ify

--- ion_phoenix.mk
+++ ion_phoenix.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/ion/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := ion_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Redmi
